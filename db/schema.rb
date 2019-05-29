c93f1e4da55109180fa819826684d8bcf5c8b1af
guillechague <guillermo.echaguea@gmail.com>
2019-05-29 19:15:23 -0400

    adding name to pins

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_29_072230) do
+ActiveRecord::Schema.define(version: 2019_05_29_222834) do
 
   create_table "pins", force: :cascade do |t|
     t.string "photo"
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
   end
 
 end
