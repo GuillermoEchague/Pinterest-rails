13723fb11b020e6054cff6458c352bef49009888
guillechague <guillermo.echaguea@gmail.com>
2019-05-31 02:43:30 -0400

    version model

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   resources :pins
   get 'welcome/index'
   root 'welcome#index'
+
+  get 'edit' => 'registrations#edit'
+  get 'pinsof/:user_id' => 'pins#pinsof', :as => "pinsof"
+  get 'mypins' => 'pins#mypins'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_30_023703) do
+ActiveRecord::Schema.define(version: 2019_05_31_043039) do
 
   create_table "pins", force: :cascade do |t|
     t.string "photo"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2019_05_30_023703) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
